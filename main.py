035fb5dc78af74f278a7e8febea7f450102180d3
appuraj-philips <98383187+appuraj-philips@users.noreply.github.com>
2022-03-05 22:14:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ class GitHubConnect():
             logging.error("Unable to update repository to internal group")
             exit(1)
 def main():
+    parser.add_argument('-pat', '--pat', help='Please provide the GitHub PAT (Persional Access Token)', required=True)
    
     github_url = "https://api.github.com"
     organisation_name = "Appu-Test-org"
